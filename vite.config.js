97f62f05960b24d432f5db38c2185f7d8c6e67c6
brokiem <milyhqvideo@gmail.com>
2022-12-08 20:31:21 +0800

    Fix website loading

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/new-tab/",
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    target: "es2015",
   }
 })
