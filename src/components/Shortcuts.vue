db7e7fa7fa86e7ee06a5283a2ba0c78fe4021bd6
brokiem <milyhqvideo@gmail.com>
2023-01-08 22:57:42 +0800

    Shortcut hover effect

--- src/components/Shortcuts.vue
+++ src/components/Shortcuts.vue
@@ -1,15 +1,13 @@
 <template>
   <div class="flex flex-wrap justify-center justify-items-center mx-5">
-    <div v-for="shortcut in shortcuts">
-      <div class="mx-6">
-        <a :href="shortcut.url">
-          <div class="text-center p-3" style="background-color: rgb(42, 42, 42)" :style="[{ 'border-radius': isRoundedFull ? '9999px' : '10px' }]">
-            <img width="28" height="28" class="rounded-lg" :src="`https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${shortcut.url}&size=64`" alt="Shortcut icon">
-          </div>
-          <div class="text-center text-xs mt-3 dark:text-white mb-7">{{ shortcut.name }}</div>
-        </a>
+    <a :href="shortcut.url" class="hover:bg-white/20 transition duration-[20ms] rounded-md" v-for="shortcut in shortcuts">
+      <div class="mx-6 mt-4">
+        <div class="text-center p-3" style="background-color: rgb(42, 42, 42)" :style="[{ 'border-radius': isRoundedFull ? '9999px' : '10px' }]">
+          <img width="28" height="28" :class="shortcut.style" :src="`https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${shortcut.url}&size=64`" alt="Shortcut icon">
+        </div>
+        <div class="text-center text-xs mt-3 dark:text-white mb-4">{{ shortcut.name }}</div>
       </div>
-    </div>
+    </a>
   </div>
 </template>
 
@@ -29,26 +27,32 @@ export default {
       youtube: {
         name: 'YouTube',
         url: 'https://www.youtube.com',
+        style: 'none'
       },
       google: {
         name: 'Google',
         url: 'https://www.google.com',
+        style: 'none'
       },
       gmail: {
         name: 'Gmail',
         url: 'https://mail.google.com',
+        style: 'none'
       },
       github: {
         name: 'GitHub',
         url: 'https://github.com',
+        style: 'rounded-full'
       },
       twitter: {
         name: 'Twitter',
         url: 'https://twitter.com',
+        style: 'none'
       },
       reddit: {
         name: 'Reddit',
         url: 'https://www.reddit.com',
+        style: 'none'
       },
     };
 
